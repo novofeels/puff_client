9fc2b0d9ded94c25d23d037af58e088703e2cd84
matt novotny <mattrnovotny@gmail.com>
2024-07-26 15:39:20 -0500

    sending posts for scores to backend, holy mother of god was getting that token to be read an ordeal

--- pages/playgame.js
+++ pages/playgame.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Unity, useUnityContext } from "react-unity-webgl";
 import Navbar from '../components/navbar';
 import styles from '../styles/playgame.module.css';
+import { useAppContext } from '../context/state';
 
 const PlayGame = () => {
+  const { token, setToken } = useAppContext();
   const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
-    loaderUrl: "/unity/Build/WebGL.loader.js",
-    dataUrl: "/unity/Build/WebGL.data",
-    frameworkUrl: "/unity/Build/WebGL.framework.js",
-    codeUrl: "/unity/Build/WebGL.wasm",
+    loaderUrl: "/unity/Build/Builds.loader.js",
+    dataUrl: "/unity/Build/Builds.data",
+    frameworkUrl: "/unity/Build/Builds.framework.js",
+    codeUrl: "/unity/Build/Builds.wasm",
   });
-
+useEffect(() => { 
+  setToken(localStorage.getItem('token'));
+}, [setToken]);
   return (
     <div className={styles.container}>
       <Navbar />
