588cdcd9bef9b1890acbf82d2248aa73879ae278
matt novotny <mattrnovotny@gmail.com>
2024-08-09 11:48:15 -0500

    again

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ module.exports = {
     output: 'export', // Ensures static HTML export
     basePath: '/puff_client', // Set this to match your GitHub repository name
     images: {
-        loader: 'default', // Use default loader which does not optimize images
-        path: '',
+     unoptimized: true,
       }
   }
   
