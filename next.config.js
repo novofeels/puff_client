30ef4dfe985b041a4a2dd503940848c196af1741
matt novotny <mattrnovotny@gmail.com>
2024-08-09 11:42:48 -0500

    turned off image optimazion

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@
 module.exports = {
     output: 'export', // Ensures static HTML export
     basePath: '/puff_client', // Set this to match your GitHub repository name
+    images: {
+        loader: 'default', // Use default loader which does not optimize images
+        path: '',
+      }
   }
   
